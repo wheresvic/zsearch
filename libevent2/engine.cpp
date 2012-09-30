bf402f67100126326d180b791a56abee728e4a93
Victor Parmar <victorparmar@gmail.com>
2012-09-30 19:24:24 -0400

    fully working engine :)

--- libevent2/engine.cpp
+++ libevent2/engine.cpp
@@ -29,8 +29,10 @@ int main()
 	// store all the documents
 	shared_ptr<IDocumentIndex> documentIndex = make_shared<DocumentIndexImpl>();
 	// inverted index that maps words(wordId) to documents that contain it
-	map<unsigned int, CompressedSet> invertedIndex;
+	map<unsigned int, string> invertedIndex;
 
+	// helper, word lookup
+	map<unsigned int, string> wordLookup;
 
 	// test input, build the documentIndex
 	while (getline(cin, input))
@@ -52,14 +54,20 @@ int main()
 		documentIndex->addDoc(docId++, doc);
 	}
 
-	// work with the data set
+	/**
+	 * we now loop through the documents
+	 * split the words up and index them
+	 * along with adding them to the inverted index
+	 */
 	auto documents = documentIndex->getDocuments();
 
 	for (auto iter = documents.begin(); iter != documents.end(); ++iter)
 	{
-		cout << "document id: " << iter->first << endl;
 
+		unsigned int docId = iter->first;
 		shared_ptr<IDocument> doc = iter->second;
+
+		cout << "document id: " << docId << endl;
 
 		// we know that there's only 1 entry for a document
 		string key = doc->getEntries().begin()->first;
@@ -72,10 +80,71 @@ int main()
 		for (auto token : qp.getTokens())
 		{
 			string word = key + keyWordSplitter + token;
-			// cout << word << endl;
-			wordIndex.insert(make_pair(word, wordId++));
+
+			auto found = wordIndex.find(word);
+			string bitmap;
+			CompressedSet set;
+
+			if (found != wordIndex.end())
+			{
+				// cout << "found!" << endl;
+
+				auto wordId = found->second;
+
+				bitmap = invertedIndex[wordId];
+				stringstream bitmapStream(bitmap);
+				set.read(bitmapStream);
+
+				// no need for unorderedAdd as we're going through the
+				// documents sequentially
+				set.addDoc(docId);
+
+				stringstream ss;
+				set.write(ss);
+				bitmap = ss.str();
+
+				invertedIndex[wordId] = bitmap;
+			}
+			else
+			{
+				// cout << "not found" << endl;
+
+				wordIndex.insert(make_pair(word, wordId++));
+				wordLookup.insert(make_pair(wordId - 1, word));
+
+				set.addDoc(docId);
+
+				stringstream ss;
+				set.write(ss);
+				bitmap = ss.str();
+
+				invertedIndex.insert(make_pair(wordId - 1, bitmap));
+			}
 		}
 
+	}
+
+	cout << "Done processing input " << endl;
+
+	for (auto iter = invertedIndex.begin(); iter != invertedIndex.end(); ++iter)
+	{
+		auto wordId = iter->first;
+		auto bitmap = iter->second;
+
+		CompressedSet set;
+		stringstream bitmapStream(bitmap);
+		set.read(bitmapStream);
+
+		cout << wordLookup[wordId] << " : ";
+
+		CompressedSet::Iterator it(&set);
+
+		for (; it.docID() != NO_MORE_DOCS; it.nextDoc())
+		{
+			cout << it.docID() << " ";
+		}
+
+		cout << endl;
 	}
 
 	return 0;
