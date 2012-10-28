dfb7b00ef70ede3e61df43a51e477bb12daaabf8
Maxime Caron <maxime.caron@gmail.com>
2012-10-28 17:33:33 -0400

    add invertedIndex interface

--- libevent2/IInvertedIndex.hpp
+++ libevent2/IInvertedIndex.hpp
@@ -1,11 +1,14 @@
 #ifndef IINVERTED_INDEX_H
 #define IINVERTED_INDEX_H
 #include "Word.hpp"
+#include "../varint/CompressedSet.h"
+
 class IInvertedIndex
 {
 	public:
-		virtual int  get(Word word, CompressedSet& set) = 0;
-		virtual void put(Word word,CompressedSet& set) = 0;
+		virtual int get(Word& word, CompressedSet& set) = 0;
+		virtual int put(Word& word, CompressedSet& set) = 0;
+		
 		// Virtual destructor
 		virtual ~IInvertedIndex() { }
 };
