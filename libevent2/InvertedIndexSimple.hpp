61e275a95e153177fd25ba22feb2841d63d3ad5e
Maxime Caron <maxime.caron@gmail.com>
2012-11-01 00:12:15 -0400

    add support for leveldb

--- libevent2/InvertedIndexSimple.hpp
+++ libevent2/InvertedIndexSimple.hpp
@@ -2,39 +2,90 @@
 #define INVERTED_INDEX_SIMPLE_H
 
 #include "IInvertedIndex.hpp"
+#include <memory>
 #include <map>
+#include <iostream>
 #include <sstream>
 #include <string>
+#include "../varint/Set.h"
 #include "../varint/CompressedSet.h"
-
+#include "KVStore.hpp"
 class InvertedIndexSimple : public IInvertedIndex
 {
-	public:
+private:
+	KVStore::KVStore store;
+	
+	char* EncodeVarint64(char* dst, uint64_t v) {
+	  static const int B = 128;
+	  unsigned char* ptr = reinterpret_cast<unsigned char*>(dst);
+	  while (v >= B) {
+	    *(ptr++) = (v & (B-1)) | B;
+	    v >>= 7;
+	  }
+	  *(ptr++) = static_cast<unsigned char>(v);
+	  return reinterpret_cast<char*>(ptr);
+	}
+
+	void PutVarint64(std::string& dst, uint64_t v) {
+	  char buf[10];
+	  char* ptr = EncodeVarint64(buf, v);
+	  dst.append(buf, ptr - buf);
+	}
+	
+	
+	void printSet(unsigned int wordId,CompressedSet& set){
+		std::cout << "wordId: [" << wordId << "]" << std::endl;
+		shared_ptr<Set::Iterator> it = set.iterator();
+		std::cout << "docid: [" ;
+		while(it->nextDoc()!= NO_MORE_DOCS) {
+				std::cout << it->docID()  << ", ";
+		
+		}
+	   std::cout << "]" << std::endl;
+	}
+		
+public:
+         InvertedIndexSimple(){
+			store.Open("/tmp/InvertedIndex");
+         }
+
 		 int get(unsigned int wordId, CompressedSet*& set) {
-			set = new CompressedSet();
-			auto found = invertedIndex.find(wordId);
-			if (found != invertedIndex.end()){
-			   string bitmap = found->second;
-			   stringstream bitmapStream(bitmap);
-			   set->read(bitmapStream);
-			   return 1;
+			string key;
+			PutVarint64(key,wordId);
+			string bitmap;
+			if(store.Get(key,bitmap).ok()){
+				stringstream bitmapStream(bitmap);
+				set = new CompressedSet();
+			    set->read(bitmapStream);
+			    return 1;
 			} else {
-			   return 0;
-			}	
+				return 0;
+			}
 		 }
 		
 		 bool exist(unsigned int wordId){
-			auto found = invertedIndex.find(wordId);
-			return found != invertedIndex.end();
+			string key;
+			PutVarint64(key,wordId);
+			string ret;
+			bool found = store.Get(key,ret).ok();
+
+			return found;
 		 };
 		
 		
 		 int put(unsigned int wordId, CompressedSet& set) {
+			string key;
+			PutVarint64(key,wordId);
+			
 			stringstream ss;
 			set.write(ss);
 			string bitmap = ss.str();
-			invertedIndex.insert(make_pair(wordId,bitmap));
-			return 1;
+			
+			if (store.Put(key,bitmap).ok()){
+				return 1;
+			} else {
+				return 0;
+			}
 		};
 		
 		int add(unsigned int wordId, unsigned int  docid){		
@@ -51,12 +102,6 @@ class InvertedIndexSimple : public IInvertedIndex
 			}
 			return 1;
 		};
-		
-
-
-	private:
-		// inverted index that maps words(wordId) to documents that contain it
-		map<unsigned int, string> invertedIndex;
 };
 
 #endif
---
+++ libevent2/KVStore.hpp
@@ -0,0 +1,118 @@
+#ifndef KVSTORE_H
+#define KVSTORE_H
+
+#include "leveldb/db.h"
+#include "leveldb/cache.h"
+#include <map>
+#include <string>
+namespace KVStore {
+class Status {
+	enum Code {
+    kOk = 0,
+    kNotFound = 1,
+    kCorruption = 2,
+    kNotSupported = 3,
+    kInvalidArgument = 4,
+    kIOError = 5
+  };
+  Code _code;
+public:
+  Code code() const {
+    return _code;
+  }
+
+  Status(Code code){
+	_code = code;
+  }
+  // Return a success status.
+  static Status OK() { return Status(kOk); }
+
+  // Return error status of an appropriate type.
+  static Status NotFound() {
+    return Status(kNotFound);
+  }
+  static Status Corruption() {
+    return Status(kCorruption);
+  }
+  static Status NotSupported() {
+    return Status(kNotSupported);
+  }
+  static Status InvalidArgument() {
+    return Status(kInvalidArgument);
+  }
+
+  static Status IOError() {
+    return Status(kIOError);
+  }
+
+  // Returns true iff the status indicates success.
+  bool ok() const { return (code() == kOk); }
+
+  // Returns true iff the status indicates a NotFound error.
+  bool IsNotFound() const { return code() == kNotFound; }
+
+  // Returns true iff the status indicates a Corruption error.
+  bool IsCorruption() const { return code() == kCorruption; }
+
+  // Returns true iff the status indicates an IOError.
+  bool IsIOError() const { return code() == kIOError; }
+};
+
+class KVStore
+{
+	leveldb::DB* db;
+	public:
+  
+		KVStore(){
+			db = NULL;
+		}
+		
+		Status Open(const std::string& path){
+			leveldb::Options options;
+		    options.create_if_missing = true;
+		    leveldb::Status status = leveldb::DB::Open(options, path, &db);
+			return Status::OK();	
+		}
+			
+		Status Put(const std::string& key,const std::string& value){
+			leveldb::Status s = db->Put(leveldb::WriteOptions(), key,value);
+			if (s.ok()) {
+				return Status::OK();	
+			} else {
+				return Status::NotFound();
+			}
+		}
+
+		Status Get(const std::string& key, std::string* value){
+			leveldb::Status s = db->Get(leveldb::ReadOptions(), key, value);
+			if (s.ok()) {
+				return Status::OK();	
+			} else {
+				return Status::NotFound();
+			}
+		}
+		
+		Status Get(const std::string& key, std::string& value){
+			return Get(key,&value);
+		}
+		
+		Status Delete(const std::string& key){
+			leveldb::Status s = db->Delete(leveldb::WriteOptions(), key);
+			if (s.ok()) {
+				return Status::OK();	
+			} else {
+				return Status::NotFound();
+			}
+		}
+		
+
+		~KVStore(){
+			delete db;	
+		}
+
+};
+
+
+} // namespace KVStore
+
+#endif
