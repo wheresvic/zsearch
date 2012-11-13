07e89ebb77b6faa1923d4bacfca08cd7f695de7c
Maxime Caron <maxime.caron@gmail.com>
2012-11-12 23:58:44 -0500

    adding the set Factory

---
+++ varint/SetFactory.h
@@ -0,0 +1,21 @@
+#ifndef SET_FACTORY_H__
+#define SET_FACTORY_H__
+#include "Set.h"
+#include "CompressedSet.h"
+#include <memory>
+class SetFactory {
+	SetFactory(){
+		
+	}
+	
+	~SetFactory{
+		
+	}
+	
+	virtual const shared_ptr<Set>&& createSparseSet(){
+		return make_shared<CompressedSet>();
+	}
+	
+}
+
+#endif //SET_FACTORY_H__
