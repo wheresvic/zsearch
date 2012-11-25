017aa30b8575e4dcc2fe80fea430a6349f2c0879
Victor Parmar <victorparmar@gmail.com>
2012-11-25 17:10:24 -0500

    building the api

---
+++ api/example.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import httplib, urllib
+import os, shutil
+
+def printText(txt):
+    lines = txt.split('\n')
+    for line in lines:
+        print line.strip()
+
+
+inputs = list()
+
+httpServ = httplib.HTTPConnection("localhost", 8080)
+httpServ.connect()
+
+path = '/home/vic/dev_prog/git/zsearch/data/load/'
+
+listing = os.listdir(path)
+
+for file in listing:
+    
+	filename = os.path.join(path, file)
+	
+	if (os.path.isfile(filename)):
+		
+		print filename
+		linestring = open(filename, 'r').read()
+		params = urllib.urlencode({'data': linestring})
+		httpServ.request('POST', '/post.htm', params)
+		# inputs.append(linestring)
+		
+		response = httpServ.getresponse()
+
+		# if response.status == httplib.OK:
+		print "Output from POST request"
+		printText (response.read())
+
+# print "loaded " + str(len(inputs)) + " files"
+
+httpServ.close()
+
