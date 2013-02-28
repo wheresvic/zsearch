3030a6e30d8fddd82f4249b7ccc0100c20ec08e9
Maxime Caron <maxime.caron@gmail.com>
2013-02-28 18:09:09 -0500

    updating bitpacksimd algorithm

--- varint/bitpacking/bitpacksimd.cpp
+++ varint/bitpacking/bitpacksimd.cpp
@@ -2,20 +2,23 @@
  * This is code is released under the
  * Apache License Version 2.0 http://www.apache.org/licenses/.
  *
- * (c) Leonid Boytsov
+ * (c) Leonid Boytsov, revised and optimized by Daniel Lemire
  */
 
-#ifndef BITPACKSIMD_H
-#define BITPACKSIMD_H
+/**
+ * Note by D. Lemire: Obviously this code requires SSE2 so
+ * when porting to non-x86 platforms, your best bet might be not
+ * to port this part of the code.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <iomanip>
+#include <stdexcept>
+
 
 #include "common.h"
-#include <immintrin.h>
-
-typedef unsigned __v4su __attribute__ ((__vector_size__ (16)));
-
-void SIMD_fastunpack_32(const __m128i *  __restrict__ in, uint32_t *  __restrict__  out, const uint32_t bit);
-void SIMD_fastpackwithoutmask_32(const uint32_t *  __restrict__ in, __m128i *  __restrict__  out, const uint32_t bit);
-void SIMD_fastpack_32(const uint32_t *  __restrict__ in, __m128i *  __restrict__  out, const uint32_t bit);
+#include "bitpacksimd.h"
 
 using namespace std;
 
@@ -18182,7 +18185,3 @@ void SIMD_fastpack_32(const uint32_t *  __restrict__ in, __m128i *  __restrict__
     throw logic_error("number of bits is unsupported");
 }
 
-
-
-
-#endif
