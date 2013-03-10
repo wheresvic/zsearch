ba8da7a5dc79d8aca1541a6c78d26480610ba2b4
Maxime Caron <maxime.caron@gmail.com>
2013-03-09 23:59:37 -0500

    adding lru cache

--- src/LRUCache.hpp
+++ src/LRUCache.hpp
@@ -4,16 +4,20 @@
 #include <list>
 #include <unordered_map>
 #include <assert.h>
-
+#include "cedar.h"
 using namespace std;
 
-template <class KEY_T, class VAL_T> class LRUCache{
+class LRUCache{
 private:
-        mutable list< pair<KEY_T,VAL_T> > item_list;
-        mutable unordered_map<KEY_T, decltype(item_list.begin()) > item_map;
+        mutable list< pair<std::string,int> > item_list; 
+        mutable cedar::da <int, -1, -2, false> trie;
+        mutable unordered_map<std::string, decltype(item_list.begin()) > item_map;
         size_t cache_size;
 private:
         void clean(void) {
+	            if (item_map.size()>cache_size){
+					std::cout << "cache full" << std::endl;
+	            }
                 while(item_map.size()>cache_size){
                         auto last_it = item_list.end(); last_it --;
                         item_map.erase(last_it->first);
@@ -25,28 +29,16 @@ public:
                 
         };
 
-        void put(const KEY_T &key, const VAL_T &val){
-                auto it = item_map.find(key);
-                if(it != item_map.end()){
-                        item_list.erase(it->second);
-                        item_map.erase(it);
-                }
-                item_list.push_front(make_pair(key,val));
-                item_map.insert(make_pair(key, item_list.begin()));
-                clean();
+        void put(const std::string &key, const int &val){
+	            trie.update (key.c_str(), key.size(), val);
+			    return;
         };
 
-        bool exist(const KEY_T &key) const {
-	            // can this be done faster ?
-    			return item_map.find(key)!=item_map.end();
-        };
-
-        VAL_T get(const KEY_T &key) const  {
-                assert(exist(key));
-                auto it = item_map.find(key);
-                // splice move the item to the front of lru list
-                item_list.splice(item_list.begin(), item_list, it->second);
-                return it->second->second;
+        bool get(const std::string &key,unsigned int& value) const  {
+	            // add bloom filter her :)
+			    const int val = trie.exactMatchSearch<int>(key.c_str(), key.size());
+				value = val;
+                return val!=-1;
         };
 };
 #endif
